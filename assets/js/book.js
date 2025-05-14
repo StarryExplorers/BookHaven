c6d4a76b52093e5d928139adab246c2a4a092e26
StarryExplorers <starryexplorers@gmail.com>
2025-05-14 16:33:43 +0500

    Update book.js

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,1 +1,15 @@
-
+fetch('data/books.json')
+  .then(response => response.json())
+  .then(data => {
+    const grid = document.querySelector('.books-grid');
+    grid.innerHTML = '';
+    data.forEach(book => {
+      const card = document.createElement('div');
+      card.className = 'book-card';
+      card.innerHTML = `
+        <img src="${book.image}" class="book-cover" />
+        <div class="book-title">${book.title}</div>
+      `;
+      grid.appendChild(card);
+    });
+  });
