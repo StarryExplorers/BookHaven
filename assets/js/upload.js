81ea73c92e6699d04e42b7e06b7a61fe7e3fb150
StarryExplorers <starryexplorers@gmail.com>
2025-05-14 16:22:13 +0500

    Update upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -1,1 +1,4 @@
-
+document.getElementById('uploadForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert("Book uploaded! (Well… not really, since we aren't connected to a backend yet.)");
+});
